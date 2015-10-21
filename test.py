b97335e50721374e6bc043832c45846da7c6748e
duye <guoyl1814@gmail.com>
2015-10-21 14:00:06 +0800

    RandomGraph

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 __author__ = 'duye'
 
-s = {'e':3, 'd':2}
-print(s.keys())
+from Graph import  *
+m= Vertex('x')
+
+list = [3, 5, '3', 5]
+dict = {'1':3, '5':'d'}
+def addTwo(val1, val2):
+    return val1 + val2
+
+def prt(val1,val2,addTw):
+    print(addTw(val1,val2))
+
+prt(3,5,addTwo)
+
+visited = set()
+visited.add(m)
+print(m.__dir__())
