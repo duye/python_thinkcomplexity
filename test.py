e86bc5270adf6a301d9e5272cd71f8877481099b
duye <guoyl1814@gmail.com>
2015-10-23 17:24:01 +0800

    add ch3 and its files

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
 __author__ = 'duye'
 
-import string
-
-def alphabet_digit_cycle():
-    d = 0
-    while True:
-        d += 1
-        for c in string.ascii_lowercase:
-            yield c+str(d)
-
-it= alphabet_digit_cycle()
-for c in it:
-    print(c)
+for i in range(2,25):
+    print(i)
